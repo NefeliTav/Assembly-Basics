c9fe4c562fdbd88821b5495c036916290fad7420
NefeliTav <46655076+NefeliTav@users.noreply.github.com>
2020-04-20 12:34:07 +0300

    Update string.s

--- string.s
+++ string.s
@@ -2,13 +2,13 @@
     .globl __start
 __start:
 
-      li $v0, 8
+      li $v0, 8                   #read string
       la $a0, buffer
       li $a1, 21
       #move $t0,$a0 #save string to t0
       syscall
 
-      li $v0, 4
+      li $v0, 4                   #print string
       syscall
 
       sb $zero,5($a0)
